bacb54e4763ed3ec1dde4bd2b5701d7a047b606a
Black Cranium <mikhail.zabolotskih@gmail.com>
2022-07-26 21:11:02 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,24 +1,47 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name, weight, cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
-    # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
+    def __init__(self, max_weight):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
+
+    def add_item(self, item: Item):
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
+        if (self.sum_weight() + item.weight) <= self.max_weight:
+            self.items.append(item)
+        else:
+            print(f"Предмет {item.name} слишком тяжелый")
+
+    def show_items(self):
+        """
+        Вывод все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        i = 0
+        for item in self.items:
+            i += 1
+            print(f"{i}: {item.show()}")
 
     def sum_weight(self):
         """
         Возвращает суммарный вес всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        return sum([item.weight for item in self.items])
 
     def sum_cost(self):
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        return sum([item.cost for item in self.items])
 
 
 # Создаем предметы
@@ -35,9 +58,6 @@ backpack.add_item(item1)
 backpack.add_item(item2)
 backpack.add_item(item3)
 backpack.add_item(item4)
-# TODO: Если предмет не помещается в рюкзак по весу - вывести сообщение "Предмет {name} слишком тяжелый",
-#  и сам предмет не должен быть добавлен в рюкзак.
-#  Если предмет помещает, то добавляем его в рюкзак.
 
 # Выводим все предметы в рюкзаке
 backpack.show_items()
