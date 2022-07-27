7e8b83bd8afbdbaaee0f03bcda938a3d507b7ab4
Black Cranium <mikhail.zabolotskih@gmail.com>
2022-07-27 22:04:42 +0300

    Update 06_deck_part2.py

--- Module3/practice/deck/06_deck_part2.py
+++ Module3/practice/deck/06_deck_part2.py
@@ -1,6 +1,17 @@
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        suit_icons = {"Diamonds": '\u2662', "Hearts": '\u2661', "Spades": '\u2660', "Clubs": '\u2663'}
+        return f"{self.value}{suit_icons[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 class Deck:
@@ -9,20 +20,20 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit))
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        return f"deck[{len(self.cards)}] {', '.join([card.to_str() for card in self.cards])}"
 
     def draw(self, x):
-        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        res = self.cards[:x]
+        self.cards = self.cards[x:]
+        return res
 
     def shuffle(self):
-        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -38,4 +49,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(", ".join([h.to_str() for h in hand]))
