705c385e4e7818394a26d00ff59b38f367803888
Black Cranium <mikhail.zabolotskih@gmail.com>
2022-07-28 22:13:05 +0300

    Update 02_task_deck.py

--- Module3/practice/deck/deck_tasks/02_task_deck.py
+++ Module3/practice/deck/deck_tasks/02_task_deck.py
@@ -1,4 +1,18 @@
 from deck_total import Card, Deck
 
-# TODO: Создайте колоду из 52 карт. Перемешайте ее. Вытяните 10 карт сверху
-#   и посчитайте карт какой/каких мастей среди вытянутых карт оказалось больше всего?
+deck = Deck()
+
+deck.shuffle()
+
+hand = deck.draw(15)
+
+suits = {Card.CLUBS: len([card for card in hand if card.suit == Card.CLUBS]),
+         Card.HEARTS: len([card for card in hand if card.suit == Card.HEARTS]),
+         Card.DIAMONDS: len([card for card in hand if card.suit == Card.DIAMONDS]),
+         Card.SPADES: len([card for card in hand if card.suit == Card.SPADES])}
+
+print(hand)
+# print(suits)
+more_suits = dict(filter(lambda item: item[1] == max(suits.values()), suits.items()))
+# print(more_suits)
+print("Много:", ", ".join(more_suits.keys()))
