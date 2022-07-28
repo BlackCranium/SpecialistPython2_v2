008864cb8ca3c404f737259db6050b1e45549999
Black Cranium <mikhail.zabolotskih@gmail.com>
2022-07-28 23:22:28 +0300

    Update 04_task_deck.py

--- Module3/practice/deck/deck_tasks/04_task_deck.py
+++ Module3/practice/deck/deck_tasks/04_task_deck.py
@@ -1,7 +1,22 @@
-from deck_total import Card, Deck
+from deck_total import Deck
 
-# TODO: Создайте две колоды, в каждой должно быть 36 карт(старшинство карт начинается с 6-ки). Перемешайте их.
-#   Вытягивайте карты парами - одну из первой колоды, вторую из второй.
-#   Если карта из первой колоды окажется больше(старше), то записываем 1:0(условно начисляем победное очко первой колоде),
-#   если карты одинаковые, то не учитываем очко никуда.
-#   Выведите итоговый счет, сравнив попарно все карты в колодах.
+
+Deck.values = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+deck1 = Deck()
+deck2 = Deck()
+# print(deck1)
+# print(deck2)
+deck1.shuffle()
+deck2.shuffle()
+score = [0, 0]
+
+while len(deck1.cards) > 0:
+    card1, = deck1.draw(1)
+    card2, = deck2.draw(1)
+    if card1 > card2:
+        score[0] += 1
+    elif card1 < card2:
+        score[1] += 1
+    # print(f"{card1} {card2} {score[0]}:{score[1]}")
+print(f"Итоговый счет: {score[0]}:{score[1]}")
